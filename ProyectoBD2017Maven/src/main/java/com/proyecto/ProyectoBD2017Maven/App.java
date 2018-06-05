efedcca7be68600060695e7f9c5a9f52d58bef54
eliana.tarazona.so <eliana.tarazona.so@LSTKBA79930.softtekosde.com>
2018-06-04 21:49:33 -0300

    Se crea Maven project

---
+++ ProyectoBD2017Maven/src/main/java/com/proyecto/ProyectoBD2017Maven/App.java
@@ -0,0 +1,13 @@
+package com.proyecto.ProyectoBD2017Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
